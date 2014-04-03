b3cbe2d2a69a411fabeaede3499237b55bd95723
Yair Ogen <yaogen@cisco.com>
2014-04-03 16:01:19 +0300

    check the support for java util logging in regards of jersey logging

--- http-server-jetty/src/test/java/com/cisco/oss/foundation/http/api/test/HttpServerRestTest.scala
+++ http-server-jetty/src/test/java/com/cisco/oss/foundation/http/api/test/HttpServerRestTest.scala
@@ -1,41 +1,85 @@
+/*
+ * Copyright 2014 Cisco Systems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.cisco.oss.foundation.http.api.test
 
-import scala.collection.JavaConversions._
-import org.scalatest.FunSuite
 import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.BeforeAndAfter
-import org.scalatest.GivenWhenThen
-import org.scalatest.FeatureSpec
-import scala.collection.immutable.Stack
-import org.eclipse.jetty.server.Server
 import javax.servlet.Servlet
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import com.nds.cab.infra.configuration.ConfigurationFactory
-import org.eclipse.jetty.client.ContentExchange
-import org.eclipse.jetty.io.ByteArrayBuffer
-import scala.collection.mutable.ListBuffer
-import com.nds.cab.infra.flowcontext.FlowContextFactory
-import com.nds.cab.infra.inet.utils.IpUtils
 import com.google.common.collect.ArrayListMultimap
 import org.eclipse.jetty.servlet.ServletHolder
 import org.eclipse.jetty.servlet.ServletContextHandler
-import java.util.EventListener
-import org.springframework.web.context.ContextLoaderListener
-import org.springframework.web.context.ConfigurableWebApplicationContext
-import org.springframework.web.context.WebApplicationContext
-import org.springframework.web.context.support.XmlWebApplicationContext
-import com.nds.cab.infra.highavailability.server.HttpServerUtil
 import org.springframework.context.support.ClassPathXmlApplicationContext
 import org.eclipse.jetty.util.ssl.SslContextFactory
-import org.eclipse.jetty.server.ssl.SslSelectChannelConnector
-import org.junit.Ignore
-import org.glassfish.jersey.servlet.ServletContainer
+import com.cisco.oss.foundation.configuration.ConfigurationFactory
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import com.cisco.oss.foundation.ip.utils.IpUtils
 import org.glassfish.jersey.server.ResourceConfig
-import com.nds.cab.infra.highavailability.HighAvailabilityClientFactory
-import com.nds.cab.infra.highavailability.server.HighAvailabilityServerFactory
+import org.glassfish.jersey.servlet.ServletContainer
+import com.cisco.oss.foundation.http.server.jetty.JettyHttpServerFactory
+import com.cisco.oss.foundation.flowcontext.FlowContextFactory
+import com.cisco.oss.foundation.http.apache.ApacheHttpClientFactory
+import com.cisco.oss.foundation.http.{HttpMethod, HttpRequest}
+import java.util.logging._
+import java.util.Date
+import java.text.MessageFormat
+import org.joda.time.{DateTimeZone, DateTime}
+import org.joda.time.format.{DateTimeFormatterBuilder, DateTimeFormatter, DateTimeFormat}
+
+//class MyLogFormatter extends Formatter {
+//
+//  val messageFormat = new MessageFormat("{3} [{0}] [{2}]: {1}: {5} {4} \n");
+//
+//  val dateFormatter = new DateTimeFormatterBuilder().appendPattern("yyyy/MM/dd HH:mm:ss.SSS").toFormatter
+//
+//  @Override
+//  def format(record: LogRecord): String = {
+//    val arguments = Array(
+//      truncateLoggerName(record.getLoggerName(),1),
+//      record.getLevel(),
+//      Thread.currentThread().getName(),
+//      (new DateTime(record.getMillis(), DateTimeZone.UTC)).toString(dateFormatter),
+//      record.getMessage(),
+//      Option(FlowContextFactory.getFlowContext).getOrElse("").toString)
+//    return messageFormat.format(arguments);
+//  }
+//
+//  def truncateLoggerName(n: String, precision: Int): String = {
+//    val parts = n.split('.')
+//    return parts.reverse.head
+//  }
+//
+//}
+
+//object HttpServerRestTest{
+//  LogManager.getLogManager().reset();
+//
+//
+//  val fh = new FileHandler("./log/http-server-jetty.log");
+//  fh.setLevel(Level.FINE);
+//  fh.setFormatter(new MyLogFormatter)
+//
+//  val globalLogger = Logger.getLogger("")
+//  globalLogger.addHandler(fh)
+//  globalLogger.setLevel(Level.SEVERE)
+//
+//
+//  Logger.getLogger("org.glassfish").setLevel(Level.FINEST)
+//
+//}
 
 @RunWith(classOf[JUnitRunner])
 //@Ignore
@@ -43,73 +87,42 @@ class HttpServerRestTest extends FeatureSpec with GivenWhenThen with BeforeAndAf
 
   val port = "3322"
   val name = "john"
-  var sslfactory:SslContextFactory = null
+  var sslfactory: SslContextFactory = null
   //  val host = localhost
 
   before {
 
-    val config = ConfigurationFactory getConfiguration ()
-    config setProperty ("service.serverTest1.http.port", port)
-    config setProperty ("service.serverTest1.https.port", port+1)
-    config setProperty ("service.serverTest1.http.requestValidityFilter.isEnabled", "true")
-    config setProperty ("service.serverTest1.http.flowContextFilter.isEnabled", "true")
-    config setProperty ("service.serverTest1.http.traceFilter.isEnabled", "true")
-    config setProperty ("service.serverTest1.http.traceFilter.textContentTypes.1", "text/plain")
-    config setProperty ("service.serverTest1.http.monitoringFilter.isEnabled", "false");
-    
-    
-    config setProperty ("service.serverTest2.http.port", port+3)
-    config setProperty ("service.serverTest2.https.port", port+4)
-    config setProperty ("service.serverTest2.http.requestValidityFilter.isEnabled", "true")
-    config setProperty ("service.serverTest2.http.flowContextFilter.isEnabled", "true")
-    config setProperty ("service.serverTest2.http.traceFilter.isEnabled", "true")
-    config setProperty ("service.serverTest2.http.traceFilter.textContentTypes.1", "text/plain")
-    config setProperty ("service.serverTest2.http.monitoringFilter.isEnabled", "false");
+//    Logger.getLogger("org.glassfish").setLevel(Level.FINEST)
 
-    
-    
-    config setProperty ("service.serverTest1-client.http.readTimeout", "30000")
-    config setProperty ("service.serverTest1-client.1.port", port)
-    config setProperty ("service.serverTest1-client.1.host", IpUtils.getLocalHost())
-    config setProperty ("service.serverTest1-clientssl.1.port", port)
-    config setProperty ("service.serverTest1-clientssl.1.host", IpUtils.getLocalHost())
-    
-    val map:ArrayListMultimap[String, Servlet] = ArrayListMultimap.create()
-//    val sh = new ServletContextHandler
+    val config = ConfigurationFactory getConfiguration()
+    config setProperty("serverTest1.http.port", port)
+    config setProperty("serverTest1.https.port", port + 1)
+    config setProperty("serverTest1.http.requestValidityFilter.isEnabled", "true")
+    config setProperty("serverTest1.http.flowContextFilter.isEnabled", "true")
+    config setProperty("serverTest1.http.traceFilter.isEnabled", "true")
+    config setProperty("serverTest1.http.traceFilter.textContentTypes.1", "text/plain")
+    config setProperty("serverTest1.http.monitoringFilter.isEnabled", "false");
+
+
+    config setProperty("service.serverTest1-client.http.readTimeout", "30000")
+    config setProperty("service.serverTest1-client.1.port", port)
+    config setProperty("service.serverTest1-client.1.host", IpUtils.getIpAddress)
+
+    val map: ArrayListMultimap[String, Servlet] = ArrayListMultimap.create()
+    //    val sh = new ServletContextHandler
     val resourceConfig = new ResourceConfig()
-    resourceConfig packages("com.nds")
+    resourceConfig packages ("com.cisco")
 
-//    sh.addServlet(new ServletHolder(new ServletContainer(resourceConfig)), "/*")
+    val servlets: ArrayListMultimap[String, Servlet] = ArrayListMultimap.create()
+    servlets.put("/*", new ServletContainer(resourceConfig))
 
-//    sh.setInitParameter("com.sun.jersey.config.property.packages", "com.nds")
-//    val webConfig = new XmlWebApplicationContext()
-//    webConfig.setConfigLocation("classpath*:/META-INF/restContext.xml")
-//    sh.setEventListeners(Array[EventListener](new ContextLoaderListener(webConfig)))
-    
-    val servlets:ArrayListMultimap[String,Servlet] = ArrayListMultimap.create()
-    servlets.put("/*",new ServletContainer(resourceConfig) )
-    
-    val sh2 = new ServletContextHandler
-    sh2.addServlet(new ServletHolder(new ServletContainer(new ResourceConfig(resourceConfig))), "/*")
-//    sh2.setInitParameter("com.sun.jersey.config.property.packages", "com.nds")
-//    val webConfig2 = new XmlWebApplicationContext()
-//    webConfig2.setConfigLocation("classpath*:/META-INF/restContext.xml")
-//    sh2.setEventListeners(Array[EventListener](new ContextLoaderListener(webConfig2)))
-    
-    
-    val context = new ClassPathXmlApplicationContext("classpath*:/META-INF/httpTestContext.xml")
-    sslfactory = context.getBean("sslContextFactory", classOf[SslContextFactory])
-    val sslSocketConnector = new SslSelectChannelConnector(sslfactory)
 
-//    HighAvailabilityServerFactory.startHttpServer("serverTest1", sh, sslSocketConnector)
-    HighAvailabilityServerFactory.startHttpServer("serverTest1", servlets)
-    HighAvailabilityServerFactory.startHttpServer("serverTest2", sh2)
+    JettyHttpServerFactory.INSTANCE.startHttpServer("serverTest1", servlets)
     Thread.sleep(1000)
   }
 
   after {
-    HighAvailabilityServerFactory.stopHttpServer("serverTest1")
-    HighAvailabilityServerFactory.stopHttpServer("serverTest2")
+    JettyHttpServerFactory.INSTANCE.stopHttpServer("serverTest1")
   }
 
   feature("test InterfacesResource") {
@@ -122,64 +135,27 @@ class HttpServerRestTest extends FeatureSpec with GivenWhenThen with BeforeAndAf
       given("server is running")
 
       when("client sends a request")
-      
+
       FlowContextFactory.createFlowContext()
 
-      val client = HighAvailabilityClientFactory createHttpClient ("service.serverTest1-client", HighAvailabilityClientFactory.STRATEGY_TYPE.FAIL_OVER)
-      val contentEx = new ContentExchange();
-      contentEx setRequestURI ("/ps/ifs")
-//      contentEx setRequestContent (new ByteArrayBuffer(name.getBytes()))
-      contentEx setMethod ("GET")
-      client send (contentEx)
+      val client = ApacheHttpClientFactory.createHttpClient("service.serverTest1-client")
+      val request = HttpRequest.newBuilder()
+        .uri("/ps/ifs")
+        .httpMethod(HttpMethod.GET)
+        .build()
+
+      val result = client.execute(request)
 
       then("the server should return a message")
 
-      contentEx waitForDone
 
-      val result = contentEx getResponseContent ()
       assert(result != null)
-      assert(result.contains("psSms"))
+      assert(result.getResponseAsString.contains("psSms"))
 
-
-      val contentEx2 = new ContentExchange();
-      contentEx2 setRequestURI ("/ps/ifs")
-      //      contentEx setRequestContent (new ByteArrayBuffer(name.getBytes()))
-      contentEx2 setMethod ("POST")
-      contentEx2 setRequestContent (new ByteArrayBuffer("Hello Hello".getBytes()))
-      contentEx2.setRequestContentType("text/plain")
-      client send (contentEx2)
-      contentEx2 waitForDone
 
     }
-    
-//    scenario("calling get method on uri /ps/ifs should get to my resource - using SSL") {
-//
-//      given("server is running")
-//
-//      when("client sends a request")
-//      
-//      FlowContextFactory.createFlowContext()
-//
-//      val client = HighAvailabilityClientFactory createHttpClient ("service.serverTest1-clientssl", FAIL_OVER, sslfactory)
-//      val contentEx = new ContentExchange();
-//      contentEx setRequestURI ("/ps/ifs")
-////      contentEx setRequestContent (new ByteArrayBuffer(name.getBytes()))
-//      contentEx setMethod ("GET")
-//      client send (contentEx)
-//
-//      then("the server should return a message")
-//
-//      contentEx waitForDone
-//
-//      val result = contentEx getResponseContent ()
-//      assert(result != null)
-//      assert(result.contains("psSms"))
-//
-//
-//
-//    }
 
-        
+
   }
 }
 
--- http-server-jetty/src/test/java/com/cisco/oss/foundation/http/api/test/InterfacesResource.java
+++ http-server-jetty/src/test/java/com/cisco/oss/foundation/http/api/test/InterfacesResource.java
@@ -1,15 +1,30 @@
+/*
+ * Copyright 2014 Cisco Systems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.cisco.oss.foundation.http.api.test;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-
+import com.cisco.oss.foundation.flowcontext.FlowContextFactory;
 import org.apache.commons.configuration.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
-import com.nds.cab.infra.flowcontext.FlowContextFactory;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/ps/ifs")
 public class InterfacesResource {
