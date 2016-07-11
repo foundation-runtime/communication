e2918c81041892d09ce0fa75f360b4c56c838081
Yair Ogen <yaogen@cisco.com>
2016-07-11 12:29:35 +0300

    fix probe url in PingServlet controller

--- http-server-api/src/main/java/com/cisco/oss/foundation/http/server/ServletConfiguration.java
+++ http-server-api/src/main/java/com/cisco/oss/foundation/http/server/ServletConfiguration.java
@@ -4,6 +4,7 @@ import com.cisco.oss.foundation.configuration.ConfigurationFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleServletHandlerAdapter;
@@ -29,5 +30,14 @@ public class ServletConfiguration {
     @Bean (name="probe")
     public PingServlet probe(){
         return new PingServlet();
+    }
+
+    @ConditionalOnExpression("#{'${${spring.application.name}.http.pingFilter.isEnabled}' != null && '${${spring.application.name}.http.pingFilter.isEnabled}'.toLowerCase().equals('false')}")
+    @Bean
+    public ServletRegistrationBean dispatcherRegistration(PingServlet pingServlet) {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                pingServlet);
+        registration.addUrlMappings("/probe/*");
+        return registration;
     }
 }
